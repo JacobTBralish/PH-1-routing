7fd457e7af7f8f6f7c65f3bf78249ea87a48825d
Jacob Bralish <jacob@Inspectah-Ill.local>
2018-08-21 11:31:01 -0700

    Add react router dom, hashrouter, and do two roughts

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import Home from './Home/Home';
-import About from '/About/About';
+import About from './About/About';
+import { Route, Link } from 'react-router-dom';
+
 
 class App extends Component {
-
-
-  componentDidMount(){
-    /* addEventListener listens for the even of a change and forceUpdate forces the page to rerender */
-    window.addEventListener('hashchange', () => this.forceUpdate())
-  }
-
 
   render() {
     return (
       <div>
         This is App.js
         {/* This allows you to immit switch and utilize the function to only use one page at once*/}
-        <div>
-          <a href = '/#> home'></a>
-          <a href = '/#/about'> about</a>
+        <nav>
+          <Link to = '/'> Home</Link>
+          <Link to = '/about'> About</Link>
 
-        </div>
-          {window.location.hash === '#/about' ? <About /> : <Home />};
+        </nav>
+
+        <main>
+          <Route exact path="/" Component= {Home} />
+          <Route path="/about" Component= {About} />
+        </main>
+  
       </div>
     );
   }
